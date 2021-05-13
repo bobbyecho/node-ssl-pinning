dcc31ecae42824549aa814c03465e2de70d4a6e6
Bobby Echo Prasetyo <bobbyechop@gmail.com>
2021-05-13 22:54:19 +0700

    feat: remove port on domains init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(fingerinc.xyz:3000 www.fingerinc.xyz:3000)
+domains=(fingerinc.xyz www.fingerinc.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
