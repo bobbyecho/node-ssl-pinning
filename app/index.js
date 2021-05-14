7a404010bea1e657831e537d4385f7042e804921
Bobby Echo Prasetyo <bobbyechop@gmail.com>
2021-05-15 04:34:41 +0700

    feat: add certs pinning

--- app/index.js
+++ app/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 const https = require('https');
+const helmet = require('helmet');
 
 const app = express();
 
+app.use(helmet)
+
 const port = 3000;
+
+const options = {
+    key: fs.readFileSync("/etc/letsencrypt/live/api.fingerinc.xyz/privkey1.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/api.fingerinc.xyz/fullchain.pem") // these paths might differ for you, make sure to copy from the certbot output
+};
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
@@ -16,3 +25,5 @@ app.get('/api', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+https.createServer(options, app).listen(8080);
