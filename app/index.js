fee64fea4b49dbf5fc5790a0b34b3bf8d47285e7
Bobby Echo Prasetyo <bobbyechop@gmail.com>
2021-05-16 15:11:18 +0700

    fix: simplify index.js

--- app/index.js
+++ app/index.js
@@ -1,33 +1,15 @@
 const express = require('express');
-const fs = require('fs');
-const https = require('https');
-const http = require('http');
-const helmet = require('helmet');
 
 const app = express();
 
 const httpPort = 3000;
-const httpsPort = 443;
-
-app.use(helmet());
-
-const httpsOptions = {
-    key: fs.readFileSync("./certs/api.fingerinc.xyz/privkey1.pem"),
-    cert: fs.readFileSync("./certs/api.fingerinc.xyz/fullchain1.pem"),
-    requestCert: true,
-    rejectUnauthorized: true
-};
-
-const httpsServer = https.createServer(httpsOptions, app);
-const httpServer = http.createServer(app);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    res.send('<h1>Hello Bitches</h1>');
 });
 
 app.get('/api', (req, res) => {
     res.send('<h1>Hello API</h1>');
 });
 
-httpServer.listen(httpPort);
-httpsServer.listen(httpsPort);
+app.listen(httpPort);
